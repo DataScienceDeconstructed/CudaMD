73b913cdf0924099961d8d7a001c1c7e8aa677e2
Clayton Davis <clayton.davis.ms@gmail.com>
2021-09-18 12:52:44 -0400

    Added kernels for sorting the particles into boxes

--- Kernels.cu
+++ Kernels.cu
@@ -8,9 +8,64 @@ void add(int n, float *x, float *y)
     y[i] = x[i] + y[i];
 }
 
+__global__
+void calc_v2(float3* v, float* rValues, int threadMax, int SystemParticles){
+  //calculates the dot product of a particle's velocity with itself
+  int index = threadMax*blockIdx.x + threadIdx.x;
+  if(index < SystemParticles){
+    rValues[index] = v[index].x * v[index].x + v[index].y * v[index].y + v[index].z * v[index].z ;
+  }
+}
 
 __global__
-void calc_v2(float3* v, float* rValues, int threadMax){
+void assign_particles2box(float3* r, int* BlockIndex, float3 BoxLengths, int3 BoxNumsXYZ, int threadMax, int SystemParticles){
+  /*
+  function figures out which box a particle is in
+  the indexing happens with a grid that only has blocks in the x direction and threads in the x direction to maximize
+  Args:
+    r: location of particles in float 3 format
+    BlockIndex: array to hold results
+    BlockLengths: the physical dimensions of a single block
+    BlocksNumXYZ: the number of blocks in each direction in the grid
+    threadMax: maximum number of threads in a block
+    
+  */
+  int particleindex = threadMax*blockIdx.x + threadIdx.x;
+  if (particleindex<SystemParticles){
+    int3 blockindexes;
+    blockindexes.x = r[particleindex].x / BoxLengths.x;
+    blockindexes.y = r[particleindex].y / BoxLengths.y;
+    blockindexes.z = r[particleindex].z / BoxLengths.z;
+  
+    int particlebox = blockindexes.x + BoxNumsXYZ.x * blockindexes.y + BoxNumsXYZ.x*BoxNumsXYZ.y*blockindexes.z;
+  
+    BlockIndex[particleindex] = particlebox ;
+  }
+}
+
+__global__
+void assign_box_particles(int* BlockIndex, int threadMax, int SystemParticles, int BoxNumTotal, int MaxPaticles_Box, int* BoxParticles){
+  
+  //get current thread's box index
   int index = threadMax*blockIdx.x + threadIdx.x;
-  rValues[index] = v[index].x * v[index].x + v[index].y * v[index].y + v[index].z * v[index].z ;
+  //check to see that there is a box that equates to the current thread
+  if (index < BoxNumTotal){
+    //got through the full array of box indices
+    int particleCount = 0; 
+    for(int i =0; i<SystemParticles; i++){
+      
+      //record the ones that belong to this thread's box
+      if(BlockIndex[i] == index){
+        BoxParticles[particleCount + MaxPaticles_Box*index] = i;
+        particleCount++;
+
+      }
+
+    }
+
+    //flag unused portion of the BoxParticles array
+    for(int i = particleCount;i<MaxPaticles_Box;i++){
+      BoxParticles[particleCount + MaxPaticles_Box*index] = -1;
+    }
+  }
 }
--- Kernels.cuh
+++ Kernels.cuh
@@ -1,4 +1,8 @@
 __global__ 
 void add(int n, float *x, float *y);
 __global__
-void calc_v2(float3* v, float* rValues, int threadMax);
+void calc_v2(float3* v, float* rValues, int threadMax, int SystemParticles);
+__global__
+void assign_particles2box(float3* r, int* BlockIndex, float3 BoxLengths, int3 BoxNumsXYZ, int threadMax, int SystemParticles);
+__global__
+void assign_box_particles(int* BlockIndex, int threadMax, int SystemParticles, int BoxNumTotal, int MaxPaticles_Box, int* BoxParticles);
